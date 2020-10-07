05d062eb3245ebacf733a0443674e93f6e383fcd
andersoncorreia <anderson-strife@outlook.com>
2020-10-06 23:52:31 -0300

    simulando inserção de novos processos

--- Escalonador.php
+++ Escalonador.php
@@ -10,9 +10,9 @@ class Escalonador {
         for( $i = 0; $i < $qtd_p; $i++){
             $this->fila_0[] = new Processo();   //adicionando novos processos na fila 0
         }
-        $this->run();
     }
-    private function run(){
+
+    public function run(){
         $processo_ativos = true;
         while($processo_ativos){
             print("\n Quantidade de processos na fila 0: ".count($this->fila_0));
@@ -23,12 +23,23 @@ class Escalonador {
                 if( $processo->execute(1)){     //executando com quantum 1
                     array_push($this->fila_1, $processo);//se ainda resta tempo de execução adicionar no final da proxima fila
                 }
+                $sort = rand(0, 10); //para adicionar procceso aleatoriamente
+                if ( $sort == 5){
+                    print("\n adcionando novo processo a fila 0");
+                    $this->addProcessos(1);
+                }
             }
             else {  // se a fila 0 está vazia
                 $processo = array_shift($this->fila_1); // tirando um processo do inicio da fila 1
                 if( $processo){
-                    if( $processo->execute(2)){        //executando com quantum 2
-                        array_push($this->fila_1, $processo);//se ainda resta tempo de execução adicionar no final da fila
+                    $sort = rand(0, 10); //para adicionar procceso aleatoriamente
+                    if ( $sort == 5){
+                        print("\n adcionando novo processo a fila 0 e interrompido processo da fila 1");
+                        $this->addProcessos(1);//
+                        array_push($this->fila_0, $processo);//adcionando processo interrompido para a fila de maior prioridade
+                    }
+                    else if( $processo->execute(2)){        //executando com quantum 2
+                            array_push($this->fila_1, $processo);//se ainda resta tempo de execução adicionar no final da fila
                     }
                 }
                 else {      // se a ultima fila esta vazia todos os processo terminaram sua execução
--- main.php
+++ main.php
@@ -3,6 +3,6 @@ require "Escalonador.php";
 
 $escalonador = new Escalonador();
 $escalonador->addProcessos(5);
-$escalonador->addProcessos(5);
+$escalonador->run();
 
 print("\n\n END.");
